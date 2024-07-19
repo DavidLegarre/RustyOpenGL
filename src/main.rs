1314cf080ce2f0060aca5635e5d3f8b358d283e8
DavidLegarre <davidls920@gmail.com>
2024-07-19 23:00:43 +0200

    Extracted steps into functions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
-use glfw::{Action, Context, Key};
+use glfw::{fail_on_errors, Action, Context, Key};
 
 const WINDOW_TITLE: &str = "Hello, Window!";
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
 
 fn main() {
-    use glfw::fail_on_errors;
+    let vertices: [f32; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
+
+    let (mut glfw, mut window, events) = init_window();
+
+    while !window.should_close() {
+        process_events(&mut glfw, &mut window, &events);
+    }
+}
+
+fn init_glfw() -> glfw::Glfw {
     let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    glfw
+}
+
+fn init_window() -> (
+    glfw::Glfw,
+    glfw::PWindow,
+    glfw::GlfwReceiver<(f64, glfw::WindowEvent)>,
+) {
+    let mut glfw = init_glfw();
+
     // Create a windowed mode window and its OpenGL context
     let (mut window, events) = glfw
         .create_window(
@@ -20,22 +45,28 @@ fn main() {
     // Make the window's context current
     window.make_current();
     window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
 
-    // Loop until the user closes the window
-    while !window.should_close() {
-        // Swap front and back buffers
-        window.swap_buffers();
+    (glfw, window, events)
+}
 
-        // Poll for and process events
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            println!("{:?}", event);
-            match event {
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    window.set_should_close(true)
-                }
-                _ => {}
+fn process_events(
+    glfw: &mut glfw::Glfw,
+    window: &mut glfw::PWindow,
+    events: &glfw::GlfwReceiver<(f64, glfw::WindowEvent)>,
+) {
+    glfw.poll_events();
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                // make sure the viewport matches the new window dimensions; note that width and
+                // height will be significantly larger than specified on retina displays.
+                unsafe { gl::Viewport(0, 0, width, height) }
             }
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
+            }
+            _ => {}
         }
     }
 }
