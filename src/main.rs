697ba3ce886fbc0bd22621a4d6373c16925b9de3
DavidLegarre <davidls920@gmail.com>
2024-07-21 20:37:39 +0000

    Starting opengl book

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,60 @@
-extern crate gl;
 extern crate glfw;
+use self::glfw::{Action, Context, Key};
 
-use glfw::{fail_on_errors, Action, Context, Key};
+extern crate gl;
 
-#[macro_use]
-mod debugging;
+use std::sync::mpsc::Receiver;
 
-const WINDOW_TITLE: &str = "Hello, Window!";
-const WINDOW_WIDTH: u32 = 800;
-const WINDOW_HEIGHT: u32 = 600;
+// settings
+const SCR_WIDTH: u32 = 800;
+const SCR_HEIGHT: u32 = 600;
 
-fn main() {
-    let (mut glfw, mut window, events) = init_window();
-
-    while !window.should_close() {
-        process_events(&mut glfw, &mut window, &events);
-        unsafe {
-            gl_check_error!();
-        }
-    }
-}
-
-fn init_glfw() -> glfw::Glfw {
-    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+pub fn main() {
+    // glfw: initialize and configure
+    // ------------------------------
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(
         glfw::OpenGlProfileHint::Core,
     ));
+    #[cfg(target_os = "macos")]
     glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
 
-    glfw
-}
-
-fn init_window() -> (
-    glfw::Glfw,
-    glfw::PWindow,
-    glfw::GlfwReceiver<(f64, glfw::WindowEvent)>,
-) {
-    let mut glfw = init_glfw();
-
-    // Create a windowed mode window and its OpenGL context
+    // glfw window creation
+    // --------------------
     let (mut window, events) = glfw
         .create_window(
-            WINDOW_WIDTH,
-            WINDOW_HEIGHT,
-            WINDOW_TITLE,
+            SCR_WIDTH,
+            SCR_HEIGHT,
+            "LearnOpenGL",
             glfw::WindowMode::Windowed,
         )
-        .expect("Failed to create GLFW window.");
+        .expect("Failed to create GLFW window");
 
-    // Make the window's context current
     window.make_current();
     window.set_key_polling(true);
     window.set_framebuffer_size_polling(true);
 
-    // Set the framebuffer size callback
-    window.set_framebuffer_size_callback(|window, width, height| unsafe {
-        let _window = window;
-        gl::Viewport(0, 0, width, height);
-    });
+    // gl: load all OpenGL function pointers
+    // ---------------------------------------
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-    (glfw, window, events)
+    // render loop
+    // -----------
+    while !window.should_close() {
+        // events
+        // -----
+        process_events(&mut window, &events);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        window.swap_buffers();
+        glfw.poll_events();
+    }
 }
 
-fn process_events(
-    glfw: &mut glfw::Glfw,
-    window: &mut glfw::PWindow,
-    events: &glfw::GlfwReceiver<(f64, glfw::WindowEvent)>,
-) {
-    glfw.poll_events();
+// NOTE: not the same version as in common.rs!
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
     for (_, event) in glfw::flush_messages(events) {
         match event {
             glfw::WindowEvent::FramebufferSize(width, height) => {
