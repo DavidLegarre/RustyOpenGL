e2ad420bdd14468dbc5940f9d1698ff3f4768ccb
DavidLegarre <davidls920@gmail.com>
2024-07-21 19:36:59 +0000

    Testing error check macro

---
+++ src/debugging/check_errors.rs
@@ -0,0 +1,28 @@
+#![allow(non_snake_case)]
+
+pub unsafe fn glCheckError_(file: &str, line: u32) -> u32 {
+    let mut error_code = gl::GetError();
+    while error_code != gl::NO_ERROR {
+        let error = match error_code {
+            gl::INVALID_ENUM => "INVALID_ENUM",
+            gl::INVALID_VALUE => "INVALID_VALUE",
+            gl::INVALID_OPERATION => "INVALID_OPERATION",
+            gl::STACK_OVERFLOW => "STACK_OVERFLOW",
+            gl::STACK_UNDERFLOW => "STACK_UNDERFLOW",
+            gl::OUT_OF_MEMORY => "OUT_OF_MEMORY",
+            gl::INVALID_FRAMEBUFFER_OPERATION => "INVALID_FRAMEBUFFER_OPERATION",
+            _ => "unknown GL error code",
+        };
+
+        println!("{} | {} ({})", error, file, line);
+
+        error_code = gl::GetError();
+    }
+    error_code
+}
+
+macro_rules! gl_check_error {
+    () => {
+        glCheckError_(file!(), line!())
+    };
+}
